f6122e54c1fc121fa1a071900396d0055d7d88d5
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-24 17:26:58 +0200

    starting styling

--- vite-boolflix/src/components/InputSearch.vue
+++ vite-boolflix/src/components/InputSearch.vue
@@ -18,16 +18,21 @@ export default {
 </script>
 
 <template>
-<div class="container mt-4">
- <div class="input-group">
-  <input v-model="resultsQuery" v-on:keyup.enter="searchRequest"  type="search" class="form-control rounded" placeholder="Search a movie..."  />
-  <button @click="searchRequest" type="button" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
- </div>
-</div>
+<header class="bg-dark d-flex justify-content-between align-items-center">
+  <div class="container">
+    <h2 class="text-danger">BOOLFLIX</h2>
+  <div class="input-group w-50">
+    <input v-model="resultsQuery" v-on:keyup.enter="searchRequest"  type="search" class="form-control rounded" placeholder="Search a movie..."  />
+    <button @click="searchRequest" type="button" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
+  </div>
+  </div>
+</header>
 </template>
 
 <style lang="scss">
+header{
+  height: 100px;
 
-
+}
 
 </style>
