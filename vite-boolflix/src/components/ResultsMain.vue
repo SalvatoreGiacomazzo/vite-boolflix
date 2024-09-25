b1c12dc43843865a1f3287675bed1209b0179676
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-25 14:06:56 +0200

    made grid system for movie cards

--- vite-boolflix/src/components/ResultsMain.vue
+++ vite-boolflix/src/components/ResultsMain.vue
@@ -19,17 +19,21 @@ voteInFive(vote){
 
 <template>
 
-<div class="container mt-4" v-for="media in medias" :key="media.id">
+<div class="container mt-4">
+ <div class="row">
+  <div class="col-md-3 mb-4" v-for="media in medias" :key="media.id">
     <div class="card" style="width: 18rem;">
         <img class="card-img-top d-block" :src="'https://image.tmdb.org/t/p/w342/' + media.poster_path" alt="Card image cap">
- <p>Titolo originale: <h4 class="text-success d-inline"> {{ media.original_title }}</h4></p>
- <p>Traduzione titolo:<h5>{{ media.title }}</h5></p>
- <p>Lingua originale:<h5>{{ media.original_language }}</h5></p>
- <p>Valutazione:<h6>{{ voteInFive(media.vote_average) }}</h6></p>
-  
+        <div class="card-body">
+            <p>Titolo originale: <span class="text-success">{{ media.original_title }}</span></p>
+            <p>Traduzione titolo: <span>{{ media.title }}</span></p>
+            <p>Lingua originale: <span>{{ media.original_language }}</span></p>
+            <p>Valutazione: <span>{{ voteInFive(media.vote_average) }}</span></p>
+        </div>
+    </div>
+  </div>
  </div>
 </div>
-  
  
 </template>
 
